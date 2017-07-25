3f0b1c28a3a63041f3cf843e94e96618b7f77185
Jackson Singleton <jsingleton12321@yahoo.com>
2017-07-25 09:47:16 -0500

    Fixed errors, moved files, need to get data retrieval working.

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ sys.path.insert(0, './src')
 from zillowapi import api, key
 
 
-address = input(
-    "What is the address without zipcode?"
-);
-postalCode = input(
-    "What is the zipcode?"
-);
+address = "3400 Pacific Ave., Marina Del Rey, CA"
+postalCode = "90292"
 
-zestimate = api.GetZEstimate(key, address, postalCode)
+result = api.GetSearchResults(key, address, postalCode)
 
-print(zestimate)
+print(result)
--- src/zillowapi.py
+++ src/zillowapi.py
@@ -1,6 +1,10 @@
 import zillow
+import os
 
-with open("../bin/zillow_key.conf", 'r') as f:
+here = os.path.dirname(os.path.abspath(__file__))
+
+filename = os.path.join(here, './zillow_key.conf')
+with open(filename, 'r') as f:
     key = f.readline().replace("\n", "")
 
 api = zillow.ValuationApi()
