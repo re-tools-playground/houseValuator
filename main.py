319e9293ef04d8d7a1f8ab071729098e9b01c679
Jackson Singleton <jsingleton12321@yahoo.com>
2017-07-25 10:18:59 -0500

    Added input. Need to clean up returned data.

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ sys.path.insert(0, './src')
 from zillowapi import api, key
 
 
-address = "2307 Fairway Pointe Drive, League City, TX"
-postalCode = "77573"
+address = raw_input("What is the address without the zipcode?")
+postalCode = raw_input("What is the zipcode?")
 
 result = api.GetSearchResults(key, address, postalCode)
 zestimate = result.get_dict()
+
+
 print(zestimate)
