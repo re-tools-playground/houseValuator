3c701cc2ec7d35e7d2064ad56558c791e88981ae
Jackson Singleton <jsingleton12321@yahoo.com>
2017-07-28 04:23:15 -0500

    Added raw value retrieval. Added more useful values. Moved GUI

--- GUI.py
+++ GUI.py
@@ -2,6 +2,8 @@ import sys
 from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import *
 from data import *
+
+
 # create our window
 app = QApplication(sys.argv)
 app.setWindowIcon(QIcon('home.png'))
@@ -19,14 +21,29 @@ postalBox.setPlaceholderText("Zip Code")
 postalBox.move(20, 70)
 postalBox.resize(280,30)
 
+#Create Address Label
+addressLabel = QLabel("Address: ", w)
+addressLabel.move(310,20)
+addressLabel.resize(330, 20)
+
+
 #Create Value Label
 value = QLabel("Value: ", w)
-value.move(310,27)
+value.move(310,50)
 value.resize(330,20)
 
-addressLabel = QLabel("Address: ", w)
-addressLabel.move(310,77)
-addressLabel.resize(330, 20)
+#Create Realistic Value Label
+realValueLabel = QLabel("Realistic Value: ", w)
+realValueLabel.move(310,80)
+realValueLabel.resize(330, 20)
+
+
+#Create Bid Label
+bidLabel = QLabel("Maximum Bid: ", w)
+bidLabel.move(310,110)
+bidLabel.resize(330, 20)
+
+
 # Set window size.
 w.setFixedSize(600, 210)
 
@@ -41,6 +58,8 @@ def on_click():
     zipCode = postalBox.text()
     value.setText("Value: " + getResults(address, zipCode))
     addressLabel.setText(getAddress(address, zipCode))
+    realValueLabel.setText("Realistic Value: $" + realisticSale(address, zipCode))
+    bidLabel.setText("Maximum Bid(@1.5x Profit): " + getMaxBid(address, zipCode))
 
     #app.exit()
 
--- data.py
+++ data.py
@@ -4,6 +4,11 @@ sys.path.insert(0, './src')
 
 from zillowapi import api, key
 
+
+def getRawResults(address, postalCode):
+    result = api.GetSearchResults(key, address, postalCode)
+    zestimate = result.zestiamte.valuation_range_low
+    return int(zestimate)
 def getResults(address, postalCode):
     result = api.GetSearchResults(key, address, postalCode)
     zestimate = result.zestiamte.valuation_range_low
@@ -16,3 +21,16 @@ def getAddress(address, postalCode):
     fullAddress = ("Address: " + address.street + ", " + address.city + ", " +
     address.state + " " + address.zipcode)
     return(fullAddress)
+
+def realisticSale(address, postalCode):
+    baseValue = getRawResults(address, postalCode)
+    realisticSaleValue = baseValue * 0.71
+    formattedValue = '{:,}'.format(realisticSaleValue)
+    return formattedValue
+
+
+def getMaxBid(address, postalCode):
+    homeValue = getRawResults(address, postalCode) * 0.71
+    maxBid = homeValue * 0.67
+    formattedBid = '{:,}'.format(maxBid)
+    return("$" + formattedBid)
